3e345e2fadfff578af2baeeedae53d8aca0595f9
Jeremy Gagon <jergagon1@gmail.com>
2015-05-04 21:23:58 -0700

    Add template via js

--- public/javascripts/articles.js
+++ public/javascripts/articles.js
@@ -10,7 +10,9 @@ $(document).ready(function(){
     console.log(response);
     for(var i=0; i < response.length; i++){
       // debugger
-      // $(".articles-list").append("<li>" + response[i] )
+      // todo replace this with handlebars
+      var template = '<li data-article-id="' + response[i].id + '">' + response[i].title + '</li>';
+      $(".articles-list").append(template);
     }
   })
   .fail(function(){
